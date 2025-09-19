9c0ef3afa3274aaf61de31d50bb9463499ecc07f
Yun Cong <yuncongfw@gmail.com>
2025-09-19 17:43:39 +0800

    Fix Data extraction index error

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,13 @@ DATASET = "backend\\CEAS_08.csv"
 @app.route("/")
 def hello():
     emailList = DatasetExtraction(5)
+    for email in emailList:
+        print(email.sender)
     return jsonify(message="Hello from Flask backend!")
 
-
-
+# @app.route("/emails")
+# def get_emails():
+#     pass
 class Email:
     def __init__(self, sender, subject, body):
         self.sender = sender
@@ -43,39 +46,40 @@ class Email:
             match = re.search(r"(?P<url>https?://[^\s]+)", self.body)
             if match:
                 print("URL found: ", match.group("url"))
-                # for email in match.group("url"):
-                #     if "bit.ly" in email or "tinyurl.com" in email or "ow.ly" in email:
-                #         self.riskScore += 1
-                #     elif ".exe" in email or ".zip" in email or ".rar" in email:
-                #         self.riskScore += 1
-                #     elif "http://" in email:
-                #         self.riskScore += 1
+                for email in match.group("url"):
+                    # checks for typical url shorteners and file extensions
+                    if "bit.ly" in email or "tinyurl.com" in email or "ow.ly" in email:
+                        self.riskScore += 1
+                    elif ".exe" in email or ".zip" in email or ".rar" in email:
+                        self.riskScore += 1
+                    #check whether url is using secure http
+                    elif "http://" in email:
+                        self.riskScore += 1
             else:
                 print("No URL found")
-        # put logic remove pass
-        pass
-
-    def Final_Risk_Score(self):
-        # put logic remove pass
-        pass
+# def Final_Risk_check(email):
+#     email.Edit_Distance_Check()
+#     email.WhiteList_Check()
+#     email.Keyword_Detection()
+#     email.Sus_Url_Detection()
 
 def DatasetExtraction(count):
     df = pd.read_csv(DATASET)
     # There are 71487 rows in the dataset
     # extracting x number of random rows from the dataset
-    random_numbers = [random.randint(0, 71486) for _ in range(count)]
+    random_numbers = [random.randint(1, 40000) for _ in range(count)]
     emailList = []
 
-    for num in random_numbers:
-        row = df.iloc[num]
-        # checks whether the data extracted from dataset is valid email or not
-        if re.Match(r"[^@]+@[^@]+\.[^@]+", row['sender']):
+    i = 0
+    while i < len(random_numbers):
+        row = df.iloc[random_numbers[i]]
+        if re.match(r"[^@]+@[^@]+\.[^@]+", row['sender']):
             emailList.append(Email(row['sender'], row['subject'], row['body']))
         else:
         #if its not valid email, extract another random row
             random_numbers.append(random.randint(max(random_numbers), 71486))
-    for email in emailList:
-        print(email.sender)
+        i += 1
+        
     return emailList
 
 if __name__ == "__main__":
